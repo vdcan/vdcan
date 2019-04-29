efb4622acaa168ef5cf6420aea26ab6e0190b082
jie cai <caijie73@hotmail.com>
2019-04-29 16:13:22 -0600

    rootscope to scope

--- Javascripts/controllers/grid.js
+++ Javascripts/controllers/grid.js
@@ -6,9 +6,9 @@ km.init = function () {
 var counter = 0;
 app.controller('MainCtrl2', [
     '$scope', '$rootScope', '$http', '$modal', function ($scope, $rootScope, $http, $modal) { 
-$rootScope.$broadcast("destory"); 
+//$rootScope.$broadcast("destory"); 
         $scope.test = function () {
-            console.log(angular.isDefined('MyLogCtrl243214'));
+        //    console.log(angular.isDefined('MyLogCtrl243214'));
         $rootScope.$broadcast("test"); 
 
     }; 
@@ -16,27 +16,36 @@ $rootScope.$broadcast("destory");
     }]);
 
 
+
 app.controller('MyLogCtrl', [
     '$scope', '$rootScope', '$http', '$modal', function ($scope, $rootScope, $http, $modal) {
         $scope.SelectedRow = {};//for getting row detail
         $scope.row = {};// for updating inserting
         $scope.ids = "";//for deleting
-        $scope.selectedRowIndex = 0;
+        $scope.selectedRowIndex = 0; 
 
+        $scope.$on('$destroy', function () {
+            console.log('Child1 is no longer necessary');
+        })
+
+        $scope.$on('test', function () {
+
+            console.log('test'+counter); counter++;
+        })
 
         var destory = false;
-        $rootScope.$on("MyLogCtrlDestory", function (event, row) {
+        $scope.$on("MyLogDestory", function (event) {
             destory = true;
         });
-        $rootScope.$on("MyLogUpdate", function (event, row) {
+        $scope.$on("MyLogUpdate", function (event, row) {
             if (!destory)
                 updateData(row)
         });
-        $rootScope.$on("MyLogInsert", function (event, row) {
+        $scope.$on("MyLogInsert", function (event, row) {
             if (!destory)
                 insertData(row);
         });
-        $rootScope.$on("MyLogDelete", function (event, id, text) {
+        $scope.$on("MyLogDelete", function (event, id, text) {
             if (!destory)
                 deleteIt(id, text)
         });
@@ -71,14 +80,12 @@ app.controller('MyLogCtrl', [
         }
         afterInsert = function (row) {
 
-            console.log($scope.gridOptions.totalItems);
-                console.log((paginationOptions.pageNumber ) * paginationOptions.pageSize)
-            if ($scope.gridOptions.totalItems >= (paginationOptions.pageNumber ) * paginationOptions.pageSize ) {
-            $scope.gridOptions.data.splice($scope.gridOptions.data.length - 1, 1);
-               
+            if ($scope.gridOptions.totalItems >= (paginationOptions.pageNumber) * paginationOptions.pageSize) {
+                $scope.gridOptions.data.splice($scope.gridOptions.data.length - 1, 1);
+
             }
 
-         $scope.gridOptions.data.unshift(row);
+            $scope.gridOptions.data.unshift(row);
             $scope.gridApi.grid.modifyRows($scope.gridOptions.data);
             $scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);
             $scope.gridApi.core.refresh();
@@ -166,12 +173,12 @@ app.controller('MyLogCtrl', [
             enableRowHeaderSelection: false,
             columnDefs:
                 [
-                    { field: 'id', displayName: 'id', width: 80, align: 'center' },
                     { field: 'action', displayName: 'action', width: 80, align: 'center' },
-                    { field: 'action_data', displayName: 'action_data', width: "*", align: 'center' },
+                    { field: 'action_data', displayName: 'action_data', width: 80, align: 'center' },
                     { field: 'add_by', displayName: 'add_by', width: 80, align: 'center' },
                     { field: 'add_on', displayName: 'add_on', width: 80, align: 'center' },
                     { field: 'app_code', displayName: 'app_code', width: 80, align: 'center' },
+                    { field: 'id', displayName: 'id', width: 80, align: 'center' },
                     { field: 'ip', displayName: 'ip', width: 80, align: 'center' },
                     { field: 'menu_code', displayName: 'menu_code', width: 80, align: 'center' },
                 ],
@@ -235,3 +242,136 @@ app.controller('MyLogCtrl', [
         getPage();
     }
 ]);
+
+
+/*
+//var app = angular.module('app', ['ui.grid', 'ui.bootstrap']);
+
+//Controller function to load the data
+app.controller('MainCtrl', function ($scope, $http, CustomerService) {
+
+    //function to be called on row edit button click
+    //Passing the selected row object as parameter, we use this row object to identify  the edited row
+    $scope.edit = function (row) {
+        //Get the index of selected row from row object
+        var index = $scope.gridOptions.data.indexOf(row);
+        //Use that to set the editrow attrbute value for seleted rows
+        $scope.gridOptions.data[index].editrow = !$scope.gridOptions.data[index].editrow;
+    };
+
+    //Method to cancel the edit mode in UIGrid
+    $scope.cancelEdit = function (row) {
+        //Get the index of selected row from row object
+        var index = $scope.gridOptions.data.indexOf(row);
+        //Use that to set the editrow attrbute value to false
+        $scope.gridOptions.data[index].editrow = false;
+        //Display Successfull message after save
+        $scope.alerts.push({
+            msg: 'Row editing cancelled',
+            type: 'info'
+        });
+    };
+
+    $scope.alerts = [];
+
+    //Class to hold the customer data
+    $scope.Customer = {
+        customerID: '',
+        companyName: '',
+        contactName: '',
+        contactTitle: ''
+    };
+
+    //Function to save the data
+    //Here we pass the row object as parmater, we use this row object to identify  the edited row
+    $scope.saveRow = function (row) {
+        //get the index of selected row 
+        var index = $scope.gridOptions.data.indexOf(row);
+        //Remove the edit mode when user click on Save button
+        $scope.gridOptions.data[index].editrow = false;
+
+        //Assign the updated value to Customer object
+        $scope.Customer.customerID = row.CustomerID;
+        $scope.Customer.companyName = row.CompanyName;
+        $scope.Customer.contactName = row.ContactName;
+        $scope.Customer.contactTitle = row.ContactTitle;
+
+        //Call the function to save the data to database
+        CustomerService.SaveCustomer($scope).then(function (d) {
+            //Display Successfull message after save
+            $scope.alerts.push({
+                msg: 'Data saved successfully',
+                type: 'success'
+            });
+        }, function (d) {
+            //Display Error message if any error occurs
+            $scope.alerts.push({
+                msg: d.data,
+                type: 'danger'
+            });
+        });
+    };
+    //Get function to populate the UI-Grid
+    $scope.GetCustomer = function () {
+        $scope.gridOptions = {
+            //Declaring column and its related properties
+            columnDefs: [
+                {
+                    name: "CustomerID", displayName: "Customer ID", field: "CustomerID",
+                    cellTemplate: '<div  ng-if="!row.entity.editrow">{{COL_FIELD}}</div><div ng-if="row.entity.editrow"><input type="text" style="height:30px" ng-model="MODEL_COL_FIELD"</div>', width: 80
+                },
+                {
+                    name: "CompanyName", displayName: "Company Name", field: "CompanyName",
+                    cellTemplate: '<div  ng-if="!row.entity.editrow">{{COL_FIELD}}</div><div ng-if="row.entity.editrow"><input type="text" style="height:30px" ng-model="MODEL_COL_FIELD"</div>', width: 200
+                },
+                {
+                    name: "ContactName", displayName: "Contact Name", field: "ContactName",
+                    cellTemplate: '<div  ng-if="!row.entity.editrow">{{COL_FIELD}}</div><div ng-if="row.entity.editrow"><input type="text" style="height:30px" ng-model="MODEL_COL_FIELD"</div>', width: 140
+                },
+                {
+                    name: "ContactTitle", displayName: "Contact Title", field: "ContactTitle",
+                    cellTemplate: '<div  ng-if="!row.entity.editrow">{{COL_FIELD}}</div><div ng-if="row.entity.editrow"><input type="text" style="height:30px" ng-model="MODEL_COL_FIELD"</div>', width: 140
+                },
+                {
+                    name: 'Actions', field: 'edit', enableFiltering: false, enableSorting: false,
+                    cellTemplate: '<div><button ng-show="!row.entity.editrow" class="btn primary" ng-click="grid.appScope.edit(row.entity)"><i class="fa fa-edit"></i></button>' +  //Edit Button
+                        '<button ng-show="row.entity.editrow" class="btn primary" ng-click="grid.appScope.saveRow(row.entity)"><i class="fa fa-floppy-o"></i></button>' +//Save Button
+                        '<button ng-show="row.entity.editrow" class="btn primary" ng-click="grid.appScope.cancelEdit(row.entity)"><i class="fa fa-times"></i></button>' + //Cancel Button
+                        '</div>', width: 100
+                }
+            ],
+            onRegisterApi: function (gridApi) {
+                $scope.gridApi = gridApi;
+            }
+        };
+        //Function load the data from database
+        CustomerService.GetCustomer().then(function (d) {
+            $scope.gridOptions.data = d.data;
+        }, function (d) {
+            alert(d.data);
+        });
+    };
+    //Call  function to load the data
+    $scope.GetCustomer();
+});
+
+//Factory
+app.factory('CustomerService', function ($http) {
+    var res = {};
+    res.GetCustomer = function () {
+        return $http({
+            method: 'GET',
+            dataType: 'jsonp',
+            url: 'api/Customer/GetCustomer'
+        });
+    }
+
+    res.SaveCustomer = function ($scope) {
+        return $http({
+            method: 'POST',
+            data: $scope.Customer,
+            url: 'api/Customer/UpdateCustomer'
+        });
+    }
+    return res;
+});*/
