6a0b38dfdf1d24d9e9e11dc485e3f63f47e0b252
jie cai <caijie73@hotmail.com>
2019-05-14 07:07:16 -0600

    side

--- Areas/ANJS/ViewJS/angular_side.js
+++ Areas/ANJS/ViewJS/angular_side.js
@@ -14,166 +14,7 @@
 var km = {};
 km.model = null;
 km.parent_model = null;
-
-km.init = function () {
-    com.initbuttons($('#km_toolbar'), km.model.buttons);
-    km.init_parent_model(); 
- //   km.template.init();
-         if (typeof Extern_Init != "undefined")	
-     Extern_Init();
  
- 
-    // if it is static combobox can be deleted 
-     $('.easyui-combobox').each(function (index, element) {
-       var options = $(this).combobox('options')
-          var u = $(this).attr("url2")
-      
-        if (u!= undefined) { 
-         
-            options.url = km.model.urls[u];
-            if (options.multiple == true) {
-
-                options.formatter= function (row) { //formatter this method is used to add options checkbox select method   
-                    var opts = $(this).combobox('options');  
-                    return '<input type="checkbox" class="combobox-checkbox">' + row[opts.textField]  
-                };
-            options.onLoadSuccess = function () {  // after select load successfuly   
-                var opts = $(this).combobox('options');  
-                var target = this;  
-                var values = $(target).combobox('getValues');// get selected value values  
-                $.map(values, function (value) {  
-                    var el = opts.finder.getEl(target, value);  
-                    el.find('input.combobox-checkbox')._propAttr('checked', true);   
-                })  
-            };
-            options.onSelect = function (row) { // when select an option   
-                var opts = $(this).combobox('options');  
-                // get selected value values  
-                //$("#"+id).val($(this).combobox('getValues'));  
-                     
-                // set check box in combobox   
-                var el = opts.finder.getEl(this, row[opts.valueField]);  
-                el.find('input.combobox-checkbox')._propAttr('checked', true);  
-            }
-            options.onUnselect =function (row) {// nothing selected   
-                var opts = $(this).combobox('options');  
-                // get selected value values  
-            //    $("#"+id).val($(this).combobox('getValues'));  
-                    
-                var el = opts.finder.getEl(this, row[opts.valueField]);  
-                el.find('input.combobox-checkbox')._propAttr('checked', false);  
-            }
-            }
-            $(this).combobox(options);
-
-        } 
-        
-          var loader = $(this).attr("loader");
-         if (loader != undefined) { 
-              
-            options.loader = function (param, success, error) {
-                var q = param.q || "";
-                if (q.length <= 0) { 
-                    return false;
-                } 
-                var jsonParam = { loader: loader, value: q } 
-                com.ajax({
-                    url: km.model.urls["loader"], data: jsonParam, success: function (result) {
-                        success(result);
-                    }
-                });
-            };
-            $(this).combobox(options); 
-         }
-
-          var sloader = $(this).attr("sloader");
-         if (sloader != undefined) {
-             options.loader = function (param, success, error) {
-                 var q = param.q || "";
-                 if (q.length <= 0) { 
-                     return false;
-                 }
-                 var jsonParam = { loader: sloader, value: q }
-                 com.ajax({
-                     url: km.model.urls["sloader"], data: jsonParam, success: function (result) {
-                         success(result);
-                     }
-                 });
-             };
-             $(this).combobox(options);
-         }
-        var ddl = $(this).attr("ddl");
-         if (ddl != undefined) { 
-             options.url = km.model.urls["ddler"]+"&ddl="+ddl;
-
-             $(this).combobox(options);
-         }
-
-
-         var sddl = $(this).attr("sddl");
-         if (sddl != undefined) {
-             options.url = km.model.urls["sddler"] + "&ddl=" + sddl;
-
-             $(this).combobox(options);
-         }
-
-    });
-    
-}
-
-/* Init iframe parent page model object app.index.js client object */
-km.init_parent_model = function () {
-    // Only in child page, it can get parent page model object  parent.wrapper.model
-    if (window != parent) {
-        if (parent.wrapper) {
-            km.parent_model = parent.wrapper.model;
-            //com.message('s', ' Got parent page model object <br>' + JSON.stringify(km.parent_model));
-        } else {
-            com.showcenter(' message ', " exist parent page, but can not get it parent.wrapper object ");
-        }
-    } else {
-        com.showcenter(' message ', " the cureent page is out of iframe ，can not get parent.wrapper Object ");
-    }
-}
-
-$(km.init);
-
-// page object parameters 
-km.settings = {};
-
-// Data format 
-km.gridformat = {};
-
-
-function showValue(value, data, type) {
-
-    var tmp = "";
-    var a = data.split(" ");
-    for (var i = 0; i < a.length; i++) {
-
-        var v= a[i].split("=")[0];
-        var  t = a[i].split("=")[1];
-        if (type == "checkbox") {
-
-            if ( (","+value+",").indexOf  (","+v+",")>=0)
-                tmp += "&nbsp;<input type='checkbox' checked  disabled='true'  >&nbsp;" + t;
-            else
-                tmp += "&nbsp;<input type='checkbox' disabled='true' >&nbsp;" + t;
-
-
-        } else if (type == "radio") {
-            if (value ==v)
-                tmp += "&nbsp;<input type='radio' checked disabled='true' >&nbsp;" + t;
-            else
-                tmp += "&nbsp;<input type='radio' disabled='true' >&nbsp;" + t;
-        } else {
-            if (value ==v)
-                return t;
-        }
-    }
-    return tmp;
-} 
-
    
  
 //------------------------------------------------------------------------------ 
@@ -182,7 +23,7 @@ function showValue(value, data, type) {
 //			   
 //------------------------------------------------------------------------------  
  
-app.controller('MyLogDetailCtrl', ['$scope', '$rootScope', '$stateParams', '$modal', '$http', function ($scope, $rootScope, $stateParams, $modal, $http) {
+app.controller('MyLogDetailCtrl3', ['$scope', '$rootScope', '$stateParams', '$modal', '$http', function ($scope, $rootScope, $stateParams, $modal, $http) {
     /*  var id = $stateParams.id;
       var number = $stateParams.number;
       console.log(id);
@@ -399,7 +240,7 @@ app.controller('MainCtrl', function ($scope, $state, $stateParams, $rootScope) {
 
        
  
-app.controller('MyLogCtrl', [
+app.controller('MyLogCtrl3', [
     '$scope', '$rootScope', '$http', '$modal','$q', function ($scope, $rootScope, $http, $modal,$q) {
         $scope.SelectedRow = {};//for getting row detail
         $scope.row = {};// for updating inserting
