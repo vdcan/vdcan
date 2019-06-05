e7872da094097d7e3bc3e196083e5919b7cfbd57
jie cai <caijie73@hotmail.com>
2019-06-05 13:34:18 -0600

    ok

--- Areas/ANJS/ViewJS/steps.js
+++ Areas/ANJS/ViewJS/steps.js
@@ -71,28 +71,56 @@ app.controller('stepsDetailCtrl', ['$scope', '$rootScope', '$stateParams', '$mod
         $(".stepsDetailButtons").show();
     });     */ 
     $scope.save = function () {
-    	
-    	
-    	  com.ajax({
-                type: 'POST', url: km.model.urls["steps_edit"], data: row, success: function (result) {
-                    if (result.s) {
-                       
-                       
-						        $(".MyLogDetailButtons").hide(); 
-						        $scope.row.editrow = false;
-        
-                    }
-                    $scope.showResult(result, "Insert");
-                }
-            });
-             
+
+        $http({
+            method: 'POST',
+            url: km.model.urls["steps_edit"],
+            data: $scope.row
+        }).then(function successCallback(response) {
+            $scope.showResult(response.data, "Save");
+
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        }); 
+         
     }
+    $scope.showResult = function (result, title) {
+        if (result.s) {
+            $rootScope.$broadcast("SysToaster", 'success', title, result.message);
+        } else {
+            $rootScope.$broadcast("SysToaster", 'error', title, result.message);
+        }
+    }
+    $scope.load = function () {
+        console.log("load");
+    	var row={id: 0}
+        console.log(km.model.urls["steps_detail"]);
+
+
+        $http({
+            method: 'POST',
+            url: km.model.urls["steps_detail"],
+            data: row
+        }).then(function successCallback(response) {
+            console.log(response.data);
+
+            $scope.row = Object.assign({}, response.data[0]);
+            $scope.row.editrow = true;
+            $scope.row_old = response.data[0];
+
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            }); 
+    }
+    $scope.load();
     $scope.cancel = function () { 
     	
         $rootScope.$broadcast("stepsCancel", $scope.row);
         $("stepsDetailButtons").hide();
         $scope.row = Object.assign({}, $scope.row_old);
-        $scope.row.editrow = false;
+        $scope.row.editrow = true;
     }
     
 }]);  
